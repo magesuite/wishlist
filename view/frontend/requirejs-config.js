4b1d8f09af5bf06cc05ad29da07fe3252432bf50
Marta Siewierska-Stańczak <marta.siewierska-stanczak@creativestyle.pl>
2022-07-19 12:25:53 +0200

    feat: Add ajax add-to-wishlist functionality for tile and PDP

---
+++ view/frontend/requirejs-config.js
@@ -0,0 +1,9 @@
+var config = {
+    config: {
+        mixins: {
+            'Magento_Wishlist/js/add-to-wishlist': {
+                'MageSuite_Wishlist/js/wishlist/add-to-wishlist-ext': true,
+            },
+        },
+    },
+};
