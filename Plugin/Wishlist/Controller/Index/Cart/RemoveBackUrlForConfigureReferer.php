2eacdc3d4ac8538726359aed436fa275c2cd88f5
Zbigniew Kuraś <zbigniew.kuras@creativestyle.pl>
2026-07-23 13:27:45 +0200

    feat: MGS-5401 fix error message during add to cart from configure wishlist item

---
+++ Plugin/Wishlist/Controller/Index/Cart/RemoveBackUrlForConfigureReferer.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MageSuite\Wishlist\Plugin\Wishlist\Controller\Index\Cart;
+
+class RemoveBackUrlForConfigureReferer
+{
+    public function __construct(
+        protected \Magento\Checkout\Helper\Cart $cartHelper,
+        protected \Magento\Framework\App\Response\RedirectInterface $redirect,
+        protected \Magento\Framework\UrlInterface $urlBuilder,
+        protected \Magento\Wishlist\Model\ItemFactory $itemFactory
+    ) {
+    }
+
+    public function afterExecute(\Magento\Wishlist\Controller\Index\Cart $subject, $result)
+    {
+        if (!$result instanceof \Magento\Framework\Controller\Result\Json) {
+            return $result;
+        }
+
+        if (!$this->shouldRemoveBackUrl($subject)) {
+            return $result;
+        }
+
+        $result->setData([]);
+
+        return $result;
+    }
+
+    protected function shouldRemoveBackUrl(\Magento\Wishlist\Controller\Index\Cart $subject): bool
+    {
+        if (!$subject->getRequest()->isAjax()) {
+            return false;
+        }
+
+        if ($this->cartHelper->getShouldRedirectToCart()) {
+            return false;
+        }
+        
+        return true;
+    }
+
+}
